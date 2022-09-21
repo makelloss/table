f9b23ceb20dd521477ac70af7d9e623528ffac7d
makelloss <artemvakhno@gmail.com>
2022-09-21 22:06:48 +0300

    +1

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
+$i1 = "<img src=\"images/1.png\"width=\"50\"\"height =\"50\">";
 print "<table style=\"border:solid;\">";
-for ($i = 0; $i <= 10; $i++) {
-    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\"></td></tr>";
-}
-for ($a = 1; $a <= 10; $a++) {
-    
-    print "<img src=\"images/1.png\"width=\"50\"\"height =\"50\">";
+for ($i = 0; $i <= rand(1,10); $i++) {
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\">$i1</td></tr>";
 }
 print "</table>";
